799e5e98c0ffb3b153f36a09df74b62395258002
Mbah Precious Inye'e <preciousmbah2005@gmail.com>
2026-08-04 23:04:52 +0100

    learn path operations

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+# Path operation or refered to a route
+@app.get("/") # Decorator
+def root(): # Function
+    return {"message": "welcome to my api!"}
+
+# request comes in with a  Get method and the url "/"
+
+@app.get("/post")
+def get_posts():
+    return {"data": "This is your posts"}
+    
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"message": "successfully created post"}
 
 
