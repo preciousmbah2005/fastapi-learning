40071c084b997a79b6e9b200f7bfef210ec0f4cc
Mbah Precious Inye'e <preciousmbah2005@gmail.com>
2026-08-07 08:31:38 +0100

    feat: implement Post model and update create_posts endpoint

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel 
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 # Path operation or refered to a route
 @app.get("/") # Decorator
@@ -16,8 +24,13 @@ def get_posts():
     return {"data": "This is your posts"}
     
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"message": "successfully created post"}
+def create_posts(post: Post):
+    print(post)
+    print(post.model_dump())
+    return {"data": post}
+
+# title str, content str
+
+
 
 
