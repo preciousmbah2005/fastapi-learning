38c5145f9417f8020fce2483eab638be618ae0ee
Mbah Precious Inye'e <preciousmbah2005@gmail.com>
2026-08-07 10:10:20 +0100

    fix: update endpoint paths and improve post handling logic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import Optional
-
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel 
+from random import randrange 
 
 app = FastAPI()
 
@@ -12,6 +12,10 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+# Storing posts in a list of dictionaries
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+
 # Path operation or refered to a route
 @app.get("/") # Decorator
 def root(): # Function
@@ -19,15 +23,18 @@ def root(): # Function
 
 # request comes in with a  Get method and the url "/"
 
-@app.get("/post")
+@app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
     
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.model_dump())
-    return {"data": post}
+    # print(post)
+    # print(post.model_dump())
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
 # title str, content str
 
